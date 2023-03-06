b0dffc80eb154385e19ae9ac6d973a81f6890c9d
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-03-06 22:17:36 +0200

    Initial Commit

---
+++ AarticlesAnimationSwifyUI/AarticlesAnimationSwifyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AarticlesAnimationSwifyUIApp.swift
+//  AarticlesAnimationSwifyUI
+//
+//  Created by Vladimir Dvornikov on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AarticlesAnimationSwifyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
